85c1c4a57c6042e5cb9751ef9125eb7a9031b4b6
Space-Paragon1 <umeasalugochidera@gmail.com>
2026-01-14 07:03:36 +0100

    chore(config): configure Next.js for webcam and media device support

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,13 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true
-  }
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
 };
 
 module.exports = nextConfig;
+
